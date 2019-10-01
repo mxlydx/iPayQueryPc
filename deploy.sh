fa9126fd12f2a7150b6d21f4dca656a01872d049
满孝磊 <manxiaolei@gagogroup.com>
2019-10-01 23:22:25 +0800

    ci

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
- #!/bin/bash
+#!/bin/bash
+WEB_PATH="/data/ci/iPayQueryPc"
+TARGET="/opt/apps/zhifucha.cn/iPayQueryPc/dist"
+DIST="/opt/apps/zhifucha.cn/iPayQueryPc/"
 echo "Start deployment"
+cd ${WEB_PATH}
 echo "pulling source code..."
 git reset --hard origin/master
 git clean -f
 git pull
-echo "pulling finished..."
 git checkout master
 npm run install
 npm run build
-rm -rf /opt/apps/zhifucha.cn/iPayQueryPc/dist
-cp -r "$WEB_PATH/dist/" /opt/apps/zhifucha.cn/iPayQueryPc/
+rm -rf ${TARGET}
+cp -r "${WEB_PATH}/dist/" ${DIST}
 echo "Finished."
